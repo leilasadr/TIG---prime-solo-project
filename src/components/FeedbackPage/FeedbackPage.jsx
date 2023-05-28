07786393cfebfcacb155075594f0f829300049ee
Leila Sadr <leilasadr54@gmail.com>
2023-05-28 13:27:17 -0700

    added a slider(input type range) and a text box for feedbacks

--- src/components/FeedbackPage/FeedbackPage.jsx
+++ src/components/FeedbackPage/FeedbackPage.jsx
@@ -1,22 +1,61 @@
 import React from "react";
 import hierarchy from './hierarchy.jpg';
 import { useSelector } from "react-redux";
+import { useState } from "react";
+import './FeedbackPage.css';
 
 function FeedbackPage(){
     // console.log(hierarchy);
+    const [value, setValue] = useState(0);
+    const MAX = 10;
+    const getBackgroundSize = () => {
+	return {
+		backgroundSize: `${(value * 100) / MAX}% 100%`,
+	};
+    };
+
     return (
       <div className="container">
-        <p>Placeholder: Do you notice any sensations that are the loudest right away?
-            <br/> 
-           Where do you feel green in your body?
-           <br/> 
-           Where do you feel yellow in your body?
-           <br/> 
-           Where do you feel red in your body?
+        <p>
+         As you get ready to reflect on where you are today, 
+         I invite you to get still, close your eyes, and focus your attention inward.
+         What's the loudest sensation right this moment? Which color could it be? Where is it in the body? 
         </p>
 
         <img src={hierarchy} alt="PVT hierarchy" height={500} width={400} />
-       
+
+        <br />
+        <br />
+
+        <p>What color do you resonate with today?</p>
+
+        <br />
+
+        <input
+        name="color_feedback"
+        type="range"
+        min="0"
+        max={MAX}
+        onChange={(e) => setValue(e.target.value)}
+        style={getBackgroundSize()}
+        value={value}
+        />
+
+        <br />
+        <br />
+
+        <textarea
+        name="text_feedback"
+        rows="5"
+        cols="30"
+        placeholder="How are you feeling today?">
+        </textarea>
+
+        <br />
+        <br />
+
+        <button>Submit</button>
+
       </div>
     )
 };
