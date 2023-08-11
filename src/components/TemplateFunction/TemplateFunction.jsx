04b368f91fd268bfacd17d82daf35e3a4945466c
Leila Sadr <leilasadr54@gmail.com>
2023-08-10 20:16:33 -0700

    fixed layout

--- src/components/TemplateFunction/TemplateFunction.jsx
+++ src/components/TemplateFunction/TemplateFunction.jsx
@@ -7,6 +7,7 @@ import {useSelector} from 'react-redux';
 function TemplateFunction(props) {
   // Using hooks we're creating local state for a "heading" variable with
   // a default value of 'Functional Component'
+  
   const store = useSelector((store) => store);
   const [heading, setHeading] = useState('Functional Component');
 
